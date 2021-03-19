ee5f2f6da67651eaa1f4a0bb065656606481c7dd
Piotr Nowak <peterek99914@interia.pl>
2021-03-19 12:51:48 +0100

    Push
    Push

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Generator()
+        {
+            RandomNumberGenerator nazwa = new RandomNumberGenerator(2137);
+            int x = nazwa.nextInt(1,29);
+            Assert.IsTrue (x >= 1 && x <= 29);
+        }
+    }
+}
